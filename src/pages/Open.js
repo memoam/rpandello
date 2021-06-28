4874350bc48ec5c71a78cea71abc9edc1050fb9e
memoam <memoangeles180@gmail.com>
2021-06-27 21:01:34 -0500

    vista 3

--- src/pages/Open.js
+++ src/pages/Open.js
@@ -2,30 +2,88 @@ import "../css/Estilos.css";
 import React, {Component} from "react";
 
 class Open extends Component{
-    state={
-        form:{
-            username:'',
-            password:''
-        }
-    }
-    handlechange=async e=>{
-        await this.setState({
-            ...this.setState.form,
-            [e.target.name]:e.target.value
-        });
-        console.log(this.state.form)
-    }
+
 
     render(){
         return(
-            <div className="container">
-            <h1 className="logo animate__animated animate__backInUp">PANDELLO <span>reto</span></h1>
-            <div className="lg">hola</div>
-            <div className="lg">hola</div>
-            <div className="lg">hola</div>
-            <div className="lg">hola</div>
+            <div>
+                <button className="b2">+ Agregar Table</button>
+
+                <div className="boxf">
+                <div className="box">
                 
+                <h3>Titulo de la Tabla</h3>
+                    <div className="box-info">                               
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        
+                    </div>
+                    <button className="b">+ Agregar nota</button>
+                </div>
+                
+
+                <div className="box">
+                <h3>Titulo de la Tabla</h3>
+                    <div className="box-info">                               
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        
+                    </div>
+                    <button className="b">+ Agregar nota</button>
+                </div>
+                <div className="box">
+                <h3>Titulo de la Tabla</h3>
+                    <div className="box-info">                               
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        
+                    </div>
+                    <button className="b">+ Agregar nota</button>
+                </div>
+               
+              
+                </div>
+                
+
             </div>
+            
+              
+            
+                   
+
+        
+
+
+            
+
+            
+       
 
 
         );
